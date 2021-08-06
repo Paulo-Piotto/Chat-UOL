965ee3f5755c0ef985dabadefeecbcb07bf15925
paulo.piotto <paulo_piotto@hotmail.com>
2021-08-06 19:43:54 -0300

    mensagens atualizando a cada 3s e indo para a ultima

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 let mensagens = [];
 
-const promise = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/messages");
+const idIntervalo = setInterval(atualizaMensagens, 3000);
 
-promise.then(pegarMensagens);
+function atualizaMensagens(){
+    const promise = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/messages");
+
+    promise.then(pegarMensagens);
+}
+
 
 function pegarMensagens(resposta){
     let mensagensServidor = resposta.data;
     for(let i=0; i < mensagensServidor.length; i++){
         mensagens[i] = mensagensServidor[i];
     }
-    console.log(mensagensServidor);
     renderizarMensagens(mensagensServidor);
 }
 
@@ -17,21 +21,29 @@ function pegarMensagens(resposta){
 function renderizarMensagens(mensagensServidor){
     const areaDeMensagens = document.querySelector("main");
     areaDeMensagens.innerHTML = "";
+    let ultimaMensagem;
 
     for(let i=0; i<mensagensServidor.length; i++){
-
+      ultimaMensagem = document.querySelector(".ultima");
+        if(ultimaMensagem !== null){
+            ultimaMensagem.classList.remove("ultima");
+            
+        }
+        
         if(mensagensServidor[i].type === "message" && mensagensServidor[i].to === "Todos"){
-            areaDeMensagens.innerHTML += `<div class="container">
+            areaDeMensagens.innerHTML += `<div class="container ultima">
                             <p><span class="hora"> (${mensagensServidor[i].time})</span><span class="usuario">${mensagensServidor[i].from}</span> Para <span class="destinatario">${mensagensServidor[i].to}</span>: ${mensagensServidor[i].text}</p>
                             </div>`
         }else if(mensagensServidor[i].type === "status"){
-            areaDeMensagens.innerHTML += `<div class="container status">
+            areaDeMensagens.innerHTML += `<div class="container status ultima">
                                           <p><span class="hora">(${mensagensServidor[i].time})</span><span class="usuario">${mensagensServidor[i].from}</span> ${mensagensServidor[i].text}</p>
                                           </div>`
         }
 
-        
+
     }
+    ultimaMensagem = document.querySelector(".ultima");
+    ultimaMensagem.scrollIntoView();    
     
 }
 
